ba1eb66cdf3bb50e0e2e603c2715ef85b1a7bc86
Adnan Oner <adnanoner@gmail.com>
2017-02-28 23:49:51 +0300

    Update node version

--- 40install_node.sh
+++ 40install_node.sh
@@ -7,10 +7,6 @@ if [ ! -L /usr/bin/node ]; then
   ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/node /usr/bin/node
 fi
 
-if [ ! -L /usr/bin/npm ]; then
-  ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm /usr/bin/npm
-fi
-
 # function error_exit
 # {
 #   eventHelper.py --msg "$1" --severity ERROR
@@ -18,16 +14,21 @@ fi
 # }
 # 
 #download and extract desired node.js version 
-# echo "checking node..."
-# OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"   && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
-#   wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
-#   tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
-# echo $OUT
+echo "checking node..."
+OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"   && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
+  wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
+  tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
+echo $OUT
 
-if [ ! -f "/opt/elasticbeanstalk/node-install/npm_updated" ]; then
-  /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install npm@$NPM_VER -g
-  touch /opt/elasticbeanstalk/node-install/npm_updated
-  echo "YAY! Updated global NPM version to `npm -v`"
-else
-  echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12"
+#make sure node binaries can be found globally
+if [ ! -L /usr/bin/node ]; then
+  ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/node /usr/bin/node
 fi
+
+# if [ ! -f "/opt/elasticbeanstalk/node-install/npm_updated" ]; then
+#   /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install npm@$NPM_VER -g
+#   touch /opt/elasticbeanstalk/node-install/npm_updated
+#   echo "YAY! Updated global NPM version to `npm -v`"
+# else
+#   echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12"
+# fi
